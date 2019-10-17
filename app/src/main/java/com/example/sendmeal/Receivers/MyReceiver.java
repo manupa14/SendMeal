be9c5b0f3a3f3075ce21ba8d3b000f4ad648be28
manupa14 <manupa4@live.com.ar>
2019-10-17 19:55:34 -0300

    Se mejora aspecto visual lista de platos.

--- app/src/main/java/com/example/sendmeal/Receivers/MyReceiver.java
+++ app/src/main/java/com/example/sendmeal/Receivers/MyReceiver.java
@@ -1,20 +1,14 @@
 package com.example.sendmeal.Receivers;
 
-import android.app.Activity;
-import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
 import com.example.sendmeal.Activities.AltaPlato;
-import com.example.sendmeal.Activities.BroadcastNotification;
 import com.example.sendmeal.Activities.ListaPlatos;
 import com.example.sendmeal.R;
 
